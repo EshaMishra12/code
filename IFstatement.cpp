edb89f9ea4ed426e1a44731281595560b72dbe9d
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-01-25 16:45:13 +0530

    Add files via upload

---
+++ IFstatement.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"Enter a number:";
+cin>>a;
+if(a%2 == 0)
+{
+cout<<"Even. . . ";
+}
+if(a%2 != 0)
+{
+cout<<"Odd. . .";
+}
+return 0;
+}
