b7dd00530e390503b3a1174ad1fcb3932f34bc2f
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-01-25 16:41:31 +0530

    Add files via upload

---
+++ ifwithgoto.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"Enter a number:";
+cin>>a;
+if(a%2 == 0)
+{
+cout<<"Even. . . ";
+goto abc;
+}
+cout<<"Odd. . .";
+abc:
+return 0;
+}
