c9200b677ca6460bbf7ce6c80b6bbebc7817bf88
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-02-08 19:37:40 +0530

    Add files via upload

---
+++ 5table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    
+    cout<<a <<"x 1" <<"="<<a*1 <<endl;
+    cout <<a <<"x2" <<"="<<a*2 <<endl;
+    cout <<a <<"x2" <<"="<<a*3 <<endl;
+    cout <<a <<"x2" <<"="<<a*4 <<endl;
+    cout <<a <<"x2" <<"="<<a*5 <<endl;
+    cout <<a <<"x2" <<"="<<a*6 <<endl;
+    cout <<a <<"x2" <<"="<<a*7<<endl;
+    cout <<a <<"x2" <<"="<<a*8 <<endl;
+    cout <<a <<"x2" <<"="<<a*9 <<endl;
+    cout <<a <<"x2" <<"="<<a*10 <<endl;
+    
+
+    return 0;
+}
