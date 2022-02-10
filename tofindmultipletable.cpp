88fc50ef149989b3bce24bca04601720c0212cf1
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-02-10 18:12:58 +0530

    Add files via upload

---
+++ tofindmultipletable.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream.h>
+
+int main()
+{
+int i=1,n;
+cout<<"Enter number: ";
+cin>>n;
+while(i<=10)
+{
+cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+i++;
+}
+return 0;
+}
