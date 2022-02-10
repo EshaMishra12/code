7eff86cdb22441f1c7efd3436c2d4e17d5521ab3
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-02-10 18:29:31 +0530

    Add files via upload

---
+++ Basicc++1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter the number :";
+    cin >> a;
+
+    cout << "Your age is" << a << "Your are good";
+    return 0;
+}
