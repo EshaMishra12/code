3db350ab5315b9e981d29b70a039e92b039ec7fc
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-02-10 18:13:46 +0530

    Add files via upload

---
+++ infinite.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int i=2,n;
+cout<<"Enter Limit:";
+cin>>n;
+L:
+cout<<i<<endl;
+i++;
+i++;
+if(i<=n)
+{
+goto L;
+}
+cout<<"\nGood Work . . .";
+return 0;
+}
