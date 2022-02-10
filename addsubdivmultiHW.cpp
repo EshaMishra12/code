15eb131d9a2b1ec177a96ad9c48d38137112b3e2
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-02-10 18:06:38 +0530

    Add files via upload

---
+++ addsubdivmultiHW.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the first digits.";
+    cin>>a;
+    cout<<"Enter your second digit.";
+    cin>>b;
+
+  
+cout<<"The addition of two number" <<"="<<a+b <<endl; 
+cout<<"The subtraction of two number" <<"="<<a-b <<endl;
+cout<<"The division of two number" <<"="<<a/b <<endl;
+cout<<"The multiplication of two number" <<"="<<a*b <<endl;
+return 0;
+}
