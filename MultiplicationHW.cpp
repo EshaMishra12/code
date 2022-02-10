9f9e9c9fdbc2f654336e43af20b60adec5a2825e
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-02-10 18:07:27 +0530

    Add files via upload

---
+++ MultiplicationHW.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<" Enter your first value :";
+    cin>>a;
+    cout<<"Enter your second value :";
+    cin>>b;
+    c=a*b;
+    cout<<"The multiplication of your values are:"<<c <<endl;
+    cout<<"The multiplication of your value :"<<a <<"*" <<b <<"=" <<c;
+    return 0;
+}
