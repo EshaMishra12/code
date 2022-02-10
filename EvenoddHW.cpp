3304bad72502adccf5a6e16be2748b8e4f7df583
EshaMishra12 <98035847+EshaMishra12@users.noreply.github.com>
2022-02-10 18:14:46 +0530

    Add files via upload

---
+++ EvenoddHW.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int e,b=2;
+
+    cout<<"Enter the number .";
+    cin>>e;
+    if(e%b==0)
+    {
+        cout<<"your number is even.";
+     }
+     else 
+     {
+         cout<<"The number is odd.";
+
+     }
+     return 0;
+}
